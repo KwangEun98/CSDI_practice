fcc6cc78df9d6129890af37f1600f50fa77d1271
KwangEun98 <113772703+KwangEun98@users.noreply.github.com>
2024-05-02 10:01:20 +0900

    Add files via upload

---
+++ model/diff_models.py
@@ -0,0 +1,261 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from linear_attention_transformer import LinearAttentionTransformer
+
+
+def get_torch_trans(heads=8, layers=1, channels=64):
+    encoder_layer = nn.TransformerEncoderLayer(
+        d_model=channels, nhead=heads, dim_feedforward=64, activation="gelu"
+    )
+    return nn.TransformerEncoder(encoder_layer, num_layers=layers)
+
+def get_linear_trans(heads=8,layers=1,channels=64,localheads=0,localwindow=0):
+
+  return LinearAttentionTransformer(
+        dim = channels,
+        depth = layers,
+        heads = heads,
+        max_seq_len = 256,
+        n_local_attn_heads = 0, 
+        local_attn_window_size = 0,
+    )
+
+def Conv1d_with_init(in_channels, out_channels, kernel_size):
+    layer = nn.Conv1d(in_channels, out_channels, kernel_size)
+    nn.init.kaiming_normal_(layer.weight)
+    return layer
+
+
+class DiffusionEmbedding(nn.Module):
+    def __init__(self, num_steps, embedding_dim=128, projection_dim=None):
+        super().__init__()
+        if projection_dim is None:
+            projection_dim = embedding_dim
+        self.register_buffer(
+            "embedding",
+            self._build_embedding(num_steps, embedding_dim / 2),
+            persistent=False,
+        )
+        self.projection1 = nn.Linear(embedding_dim, projection_dim)
+        self.projection2 = nn.Linear(projection_dim, projection_dim)
+
+    def forward(self, diffusion_step):
+        x = self.embedding[diffusion_step]
+        x = self.projection1(x)
+        x = F.silu(x)
+        x = self.projection2(x)
+        x = F.silu(x)
+        return x
+
+    def _build_embedding(self, num_steps, dim=64):
+        steps = torch.arange(num_steps).unsqueeze(1)  # (T,1)
+        frequencies = 10.0 ** (torch.arange(dim) / (dim - 1) * 4.0).unsqueeze(0)  # (1,dim)
+        table = steps * frequencies  # (T,dim)
+        table = torch.cat([torch.sin(table), torch.cos(table)], dim=1)  # (T,dim*2)
+        return table
+
+
+class diff_CSDI(nn.Module):
+    def __init__(self, config, inputdim=2, mode = 'standard'):
+        super().__init__()
+        self.channels = config["channels"]
+
+        self.diffusion_embedding = DiffusionEmbedding(
+            num_steps=config["num_steps"],
+            embedding_dim=config["diffusion_embedding_dim"],
+        )
+
+        self.input_projection = Conv1d_with_init(inputdim, self.channels, 1)
+        self.output_projection1 = Conv1d_with_init(self.channels, self.channels, 1)
+        self.output_projection2 = Conv1d_with_init(self.channels, 1, 1)
+
+        nn.init.zeros_(self.output_projection2.weight)
+
+        self.residual_layers = nn.ModuleList(
+            [
+                ResidualBlock(
+                    side_dim=config["side_dim"],
+                    channels=self.channels,
+                    diffusion_embedding_dim=config["diffusion_embedding_dim"],
+                    nheads=config["nheads"],
+                    is_linear=config["is_linear"],
+                    mode = mode
+                )
+                for _ in range(config["layers"])
+            ]
+        )
+
+    def forward(self, x, cond_info, diffusion_step):
+        B, inputdim, K, L = x.shape
+
+        x = x.reshape(B, inputdim, K * L)
+        x = self.input_projection(x)
+        x = F.relu(x)
+        x = x.reshape(B, self.channels, K, L)
+
+        diffusion_emb = self.diffusion_embedding(diffusion_step)
+
+        skip = []
+        for layer in self.residual_layers:
+            x, skip_connection = layer(x, cond_info, diffusion_emb)
+            skip.append(skip_connection)
+
+        x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
+        x = x.reshape(B, self.channels, K * L)
+        x = self.output_projection1(x)  # (B,channel,K*L)
+        x = F.relu(x)
+        x = self.output_projection2(x)  # (B,1,K*L)
+        x = x.reshape(B, K, L)
+        return x
+    
+class Flatten_Head(nn.Module):
+    def __init__(self, individual = True, K = 48, nf = 256, L = 35, head_dropout=0):
+        super().__init__()
+        
+        self.individual = individual
+        self.K = K
+        self.L = L
+        
+        if self.individual:
+            self.linears = nn.ModuleList()
+            self.dropouts = nn.ModuleList()
+            self.flattens = nn.ModuleList()
+            for i in range(self.K):
+                self.flattens.append(nn.Flatten(start_dim=-2))
+                self.linears.append(nn.Linear(nf, L))
+                self.dropouts.append(nn.Dropout(head_dropout))
+        else:
+            self.flatten = nn.Flatten(start_dim=-2)
+            self.linear = nn.Linear(nf, L)
+            self.dropout = nn.Dropout(head_dropout)
+            
+    def forward(self, x):                                 # x: (bs*channel) x k x d_model x patch_length
+        if self.individual:
+            x_out = []
+            for i in range(self.K):
+                z = self.flattens[i](x[:,i,:,:])          # z: bs x channel * patch_length
+                z = self.linears[i](z)                    # z: bs x L
+                z = self.dropouts[i](z)
+                x_out.append(z)
+            x = torch.stack(x_out, dim=1)                 # x: bs x K x L
+        else:
+            x = self.flatten(x)
+            x = self.linear(x)
+            x = self.dropout(x)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, side_dim, channels, diffusion_embedding_dim, nheads, is_linear=False, mode = 'standard', patch_length = 4, stride = 2):
+        super().__init__()
+        self.diffusion_projection = nn.Linear(diffusion_embedding_dim, channels)
+        self.cond_projection = Conv1d_with_init(side_dim, 2 * channels, 1)
+        self.mid_projection = Conv1d_with_init(channels, 2 * channels, 1)
+        self.output_projection = Conv1d_with_init(channels, 2 * channels, 1)
+
+        self.is_linear = is_linear
+        self.mode = mode
+        self.patch_length = patch_length
+        self.stride = stride
+        self.flatten_head = Flatten_Head()
+        
+        self.patch_conv = nn.Conv1d(in_channels = channels, out_channels = channels, kernel_size = self.patch_length, stride = self.stride)
+        self.projection_linear = nn.LazyLinear(48)
+
+        if is_linear:
+            self.time_layer = get_linear_trans(heads=nheads,layers=1,channels=channels)
+            self.feature_layer = get_linear_trans(heads=nheads,layers=1,channels=channels)
+            self.patch_layer = get_linear_trans(heads=nheads,layers=1,channels=channels)
+        else:
+            self.time_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
+            self.feature_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
+            self.patch_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
+
+
+    def forward_time(self, y, base_shape):
+        B, channel, K, L = base_shape
+        if L == 1:
+            return y
+        y = y.reshape(B, channel, K, L).permute(0, 2, 1, 3).reshape(B * K, channel, L)
+
+        if self.is_linear:
+            y = self.time_layer(y.permute(0, 2, 1)).permute(0, 2, 1)
+        else:
+            y = self.time_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
+        y = y.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)
+        return y
+    
+    def forward_patch(self, y, base_shape):
+        B, channel, K, L = base_shape
+        if L == 1:
+            return y
+        y = y.reshape(B, channel, K, L).permute(0,2,1,3).reshape(B * K, channel, L)
+        y = self.patch_conv(y) # (bs * k) x channel x patch_length
+        # y = y.unfold(dimension = -1, size = self.patch_length, step = self.stride) # (bs * k) x channel x patch_length x patch_num
+        # y = y.reshape(B * K, channel, -1) # (bs * k) x channel x (patch_length x patch_num)
+
+
+        if self.is_linear:
+            y = self.patch_layer(y.permute(0, 2, 1)).permute(0, 2, 1)
+        else:
+            y = self.patch_layer(y.permute(0,2,1)).permute(0, 2, 1) # (bs * k) x channel x (patch_num * patch_length)
+            y = self.projection_linear(y)
+
+        y = y.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)      
+
+        # y = self.flatten_head(y) # [(bs*channel) x K x L]
+        # y = y.reshape(B, channel, K, L).reshape(B, channel, K*L)
+        return y
+
+    def forward_feature(self, y, base_shape):
+        B, channel, K, L = base_shape
+        if K == 1:
+            return y
+        y = y.reshape(B, channel, K, L).permute(0, 3, 1, 2).reshape(B * L, channel, K)
+        if self.is_linear:
+            y = self.feature_layer(y.permute(0, 2, 1)).permute(0, 2, 1)
+        else:
+            y = self.feature_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
+        y = y.reshape(B, L, channel, K).permute(0, 2, 3, 1).reshape(B, channel, K * L)
+        return y
+
+    def forward(self, x, cond_info, diffusion_emb):
+        B, channel, K, L = x.shape
+        base_shape = x.shape
+        x = x.reshape(B, channel, K * L)
+
+        diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
+        y = x + diffusion_emb
+
+        if self.mode == 'standard':
+            y = self.forward_time(y, base_shape)
+            y = self.forward_feature(y, base_shape)  # (B,channel,K*L)
+
+        elif self.mode == 'time_only':
+            y = self.forward_time(y, base_shape)
+
+        elif self.mode == 'feature_only':
+            y = self.forward_feature(y, base_shape)
+
+        elif self.mode == 'patch':
+            y = self.forward_patch(y, base_shape)
+            # y = self.forward_time(y, base_shape)
+
+        y = self.mid_projection(y)  # (B,2*channel,K*L)
+
+        _, cond_dim, _, _ = cond_info.shape
+        cond_info = cond_info.reshape(B, cond_dim, K * L)
+        cond_info = self.cond_projection(cond_info)  # (B,2*channel,K*L)
+        y = y + cond_info
+
+        gate, filter = torch.chunk(y, 2, dim=1)
+        y = torch.sigmoid(gate) * torch.tanh(filter)  # (B,channel,K*L)
+        y = self.output_projection(y)
+
+        residual, skip = torch.chunk(y, 2, dim=1)
+        x = x.reshape(base_shape)
+        residual = residual.reshape(base_shape)
+        skip = skip.reshape(base_shape)
+        return (x + residual) / math.sqrt(2.0), skip
